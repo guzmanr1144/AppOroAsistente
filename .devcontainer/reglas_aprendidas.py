2ecda2d9ecab90a202d251df6d2852cc404593a9
guzmanr1144 <guzmanr1144@gmail.com>
2026-05-04 18:10:38 -0400

    Create reglas_aprendidas.py

---
+++ .devcontainer/reglas_aprendidas.py
@@ -0,0 +1,11 @@
+# logic_reparaciones.py
+import pandas as pd
+
+def aplicar_limpieza_dinamica(df):
+    """
+    Este espacio lo llena la IA automáticamente al presionar 'Reparar'.
+    """
+    # --- INICIO DE REGLAS ---
+    
+    # --- FIN DE REGLAS ---
+    return df
