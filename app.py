e52923159c1de582905911762bf194dd40a52888
guzmanr1144 <guzmanr1144@gmail.com>
2026-05-10 19:18:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ st.markdown("""
 # UTILIDADES JSON & FUNCIONES IA
 # ==========================================
 def extraer_json_seguro(texto, es_lista=False):
-    t = texto.replace("```json", "").replace("
+t = texto.replace("```json", "").replace("
 ```", "").strip()
     c1, c2 = ("[", "]") if es_lista else ("{", "}")
     inicio = t.find(c1)
